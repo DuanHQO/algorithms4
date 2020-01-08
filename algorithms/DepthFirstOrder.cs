1d42bb4e4e7f7b25ae4bb0e475aeced4c6889702
哦也段 <491882769@qq.com>
2020-01-08 16:24:14 +0800

    有向图中基于深度优先搜索的顶点排序

---
+++ algorithms/DepthFirstOrder.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace algorithms {
+    class DepthFirstOrder {
+        public bool[] Marked;
+        public Queue<int> Pre { get; private set; }
+        public Queue<int> Post { get; private set; }
+        public Stack<int> ReversePost { get; private set; }
+
+        public DepthFirstOrder(Digraph G) {
+            Pre = new Queue<int>();
+            Post = new Queue<int>();
+            ReversePost = new Stack<int>();
+
+            Marked = new bool[G.V];
+            for (int v = 0; v < G.V; v++) {
+                if (!Marked[v])
+                    DFS(G, v);
+            }
+        }
+
+        private void DFS(Digraph g, int v) {
+            Pre.Enqueue(v);
+            Marked[v] = true;
+            foreach (var w in g.Adj(v)) {
+                if (!Marked[w])
+                    DFS(g, w);
+            }
+            Post.Enqueue(v);
+            ReversePost.Push(v);
+        }
+
+    }
+}
