666945b14a33c042ee0744a9a4f1166e880eee23
哦也段 <491882769@qq.com>
2020-01-09 22:47:07 +0800

    新增有向图的API、添加计算强连通分量的API

--- algorithms/Digraph.cs
+++ algorithms/Digraph.cs
@@ -6,11 +6,15 @@ namespace algorithms {
 
         public int V { get; private set; }
         public int E { get; private set; }
+        public int[] InDegree { get; private set; }
+        public int[] OutDegree { get; private set; }
         private List<int>[] adj;
 
         public Digraph(int v) {
             this.V = v;
             this.E = 0;
+            InDegree = new int[v];
+            OutDegree = new int[v];
             adj = new List<int>[v];
             for (int i = 0; i < v; i++) {
                 adj[i] = new List<int>();
@@ -22,13 +26,18 @@ namespace algorithms {
             for (int i = 0; i < e; i++) {
                 var v = reader.Read();
                 var w = reader.Read();
-                AddEdge(v, w);
+                //不允许自环和平行边
+                if(v != w && !adj[v].Contains(w)) {
+                    AddEdge(v, w);
+                }
             }
         }
 
         //v->w
         public void AddEdge(int v, int w) {
             adj[v].Add(w);
+            InDegree[w] += 1;
+            OutDegree[v] += 1;
             E++;
         }
 
@@ -41,11 +50,42 @@ namespace algorithms {
         public Digraph Reverse() {
             var r = new Digraph(V);
             for (int v = 0; v < V; v++) {
-                foreach (var item in adj[v]) {
-                    r.AddEdge(item, v);
+                foreach (var w in adj[v]) {
+                    r.AddEdge(w, v);
                 }
             }
             return r;
+        }
+
+        public bool HasEdge(int v, int w) {
+            foreach (var item in adj[v]) {
+                if (item == w) return true;
+            }
+            return false;
+        }
+
+        public IEnumerable<int> Sources() {
+            var sources = new List<int>();
+            for (int i = 0; i < InDegree.Length; i++) {
+                if(InDegree[i] == 0) {
+                    sources.Add(i);
+                }
+            }
+            return sources;
+        }
+
+        public IEnumerable<int> Sinks() {
+            var sinks = new List<int>();
+            for (int i = 0; i < OutDegree.Length; i++) {
+                if (OutDegree[i] == 0) {
+                    sinks.Add(i);
+                }
+            }
+            return sinks;
+        }
+
+        public bool IsMap() {
+            return false;
         }
 
         public override string ToString() {
---
+++ algorithms/KosarajuSCC.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace algorithms {
+    class KosarajuSCC {
+
+        public bool[] Marked;
+        private int[] id;
+        private int count;
+
+        public KosarajuSCC(Digraph G) {
+            Marked = new bool[G.V];
+            id = new int[G.V];
+            var order = new DepthFirstOrder(G.Reverse());
+
+            foreach (var w in order.ReversePost) {
+                if (!Marked[w]) {
+                    DFS(G, w);
+                    count++;
+                }
+            }
+        }
+
+        private void DFS(Digraph G, int v) {
+            Marked[v] = true;
+            id[v] = count;
+            foreach (var w in G.Adj(v)) {
+                if (!Marked[w])
+                    DFS(G, w);
+            }
+        }
+
+        public bool StronglyConnected(int v, int w) {
+            return id[v] == id[w];
+        }
+
+
+        public int Count() {
+            return count;
+        }
+    }
+}
